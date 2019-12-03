792875a876d3ae12dcbf5872ce86284821b6a0a7
Mateusz Kojro <mateusz.kojro@unilodz.eu>
2019-12-03 08:32:45 +0100

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+function red(){
+    document.getElementById("tekst").style.backgroundColor = "red";
+}
+function blue(){
+    document.getElementById("tekst").style.backgroundColor = "blue";
+}
+function big(){
+    document.getElementById("tekst").style.fontSize = "30px";
+}
+function blue(){
+    document.getElementById("tekst").style.fontSize = "100px";
+}
